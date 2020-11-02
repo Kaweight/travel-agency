68071c9fc44a54606026dc7cd693f3eddb6071d2
Kamil Mazelanik <kamilmaz9206@gmail.com>
2020-11-02 23:32:02 +0100

    add display options

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -3,8 +3,16 @@ import OrderSummary from '../../features/OrderSummary/OrderSummary';
 import { Row, Col } from 'react-flexbox-grid';
 import PropTypes from 'prop-types';
 
+import pricing from '../../../data/pricing.json';
+import OrderOption from '../OrderOption/OrderOption';
+
 const OrderForm = ({ tripCost, options }) => (
   <Row>
+    {pricing.map(option => (
+      <Col md={4} key={option.id}>
+        <OrderOption {...option} />
+      </Col>
+    ))}
     <Col xs={12}>
       <OrderSummary tripCost={tripCost} options={options} />
     </Col>
