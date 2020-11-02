2b1d6373d1f7bddaa8a4d9eac2abe7cfc81aa2f9
Kamil Mazelanik <kamilmaz9206@gmail.com>
2020-11-02 09:25:52 +0100

    adding an order price display

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import OrderSummary from '../../features/OrderSummary/OrderSummary';
 import { Row, Col } from 'react-flexbox-grid';
+import PropTypes from 'prop-types';
 
-const OrderForm = (tripCost, options) => (
+const OrderForm = ({ tripCost, options }) => (
   <Row>
     <Col xs={12}>
       <OrderSummary tripCost={tripCost} options={options} />
     </Col>
   </Row>
 );
+
+OrderForm.propTypes = {
+  tripCost: PropTypes.any,
+  options: PropTypes.any,
+};
 
 export default OrderForm;
